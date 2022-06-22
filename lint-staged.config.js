07751db9329604c05fbc3dc99714edaac84925f1
Farid Hernandez <farid.hdz.64@gmail.com>
2022-06-21 23:49:41 -0600

    feat: Setting up lint-staged/commitlint configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
 // const { lintStagedBaseConfig } = require('@2600hz/commio-native-utilities');
 
 module.exports = {
-  'src/**/*.{js,ts,jsx,tsx}': ['yarn run prettify', () => 'tsc --noEmit'],
+  '*.ts': ['yarn run prettify', () => 'tsc --noEmit'],
 };
